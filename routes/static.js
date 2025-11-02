4c258d02225caa1e65d322c6f9ee35f3673074ba
K-Marple <taeshinoberu@outlook.com>
2025-11-01 21:43:45 -0700

    layout and css

--- routes/static.js
+++ routes/static.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* Static Routes */
 router.use(express.static("public"));
-router.use("/css", express.static(__dirname, "public/css"));
+router.use("/css", express.static(__dirname + "public/css"));
 
 /* Exports */
 module.exports = router;
